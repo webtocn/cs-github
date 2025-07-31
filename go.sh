e49ccc92ec67fc1c0c519cd6923905ecdc2501c9
云乾 <yunqian@cncfstack.com>
2025-07-31 16:09:55 +0800

    Date=>2025年 7月31日 星期四 16时09分55秒 CST

--- go.sh
+++ go.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -x
 
 NUM=$1
+NUM=${NUM:=1}  # 若参数为空则默认为1
 
-NUM=${NUM:=1}
-
-
-for n in `echo $NUM`
+# 使用C语言风格循环
+for ((n=0; n<$NUM; n++))
 do
-    DATE=`date`
+    DATE=$(date)
     echo "Date=>$DATE" > .date
     git add . --all && git commit -m "Date=>$DATE" && git push
     sleep 1
