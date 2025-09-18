4b9575ea4fd874c9647de837aa5756468024ea41
云乾 <yunqian@cncfstack.com>
2025-09-18 10:59:10 +0800

    file

--- go.sh
+++ go.sh
@@ -20,33 +20,7 @@ run_task(){
 }
 
 
-case $ARGS in
-    sc)
-        generate_diff
-        git add . --all && git commit -m "SYNC_CHARTMUSEUM" && git push
-        ;;
-    sa)
-        generate_diff
-        git add . --all && git commit -m "SYNC_ARTIFACTHUB" && git push
-        ;;
-    task)
-        generate_diff
-        run_task 1
-        ;;
-    task5)
-        generate_diff
-        run_task 5
-        ;;
-    task10)
-        generate_diff
-        run_task 10
-        ;;
-    *)
-        echo "Usage: $0 {option}"
-        echo "  sc: To ChartMuseum 将仓库中的所有版本chart同步到存储"
-        echo "  sa: From Artifacthub 获取仓库信息"
-        echo "  task: 运行单次任务"
-        echo "  task5: 运行5次任务"
-        echo "  task10: 运行10次任务"
-        ;;
-esac
+
+generate_diff
+
+git add . --all && git commit -m "$ARGS" && git push
