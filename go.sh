451a51d51ef8f24c4b09020d1d98d2822851d6a4
云乾 <yunqian@cncfstack.com>
2025-09-18 11:01:05 +0800

    TASK_INFO=file

--- go.sh
+++ go.sh
@@ -2,6 +2,13 @@
 
 ARGS=$1
 
+if [ -z $ARGS ];then
+   echo "请输入Commit参数"
+   echo "$0 file     执行全部file待处理任务"
+   echo "$0 file-10  执行任务ID为10的待处理任务"
+   exit 0
+fi
+
 generate_diff(){
     DATE=$(date)
     echo "Date=>$DATE" > .date
@@ -19,8 +26,6 @@ run_task(){
     done
 }
 
-
-
 generate_diff
 
-git add . --all && git commit -m "$ARGS" && git push
+git add . --all && git commit -m "TASK_INFO=$ARGS" && git push
